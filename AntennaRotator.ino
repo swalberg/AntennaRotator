2ae32b88c0ea14791c4240519a14ff40728a3652
Sean <sean.walberg@nfl.com>
2023-12-11 15:11:02 -0500

    Initial import

---
+++ AntennaRotator.ino
@@ -0,0 +1,293 @@
+// Import required libraries
+#include <ArduinoOTA.h>
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESP8266mDNS.h>
+#include <TinyXML.h>
+#include <WebSerial.h>
+#include <WiFiUdp.h>
+#include "RunningAverage.h"
+#include "pages.h"
+
+
+
+const char* hostname = "rotator";
+const int MAX_UDP_PACKET_SIZE = 255;
+const char* PARAM_OUTPUT = "output";
+const char* PARAM_STATE = "state";
+const int runningAverageSize = 25;
+int desiredHeading = 0;
+bool moving = false;
+unsigned long startedMovingAtMillis;
+const unsigned long watchdogTimeoutMillis = 90 * 1000;  // timeout in ms
+
+WiFiUDP RLInfo;
+unsigned int localUdpPort = 12060;
+char incomingPacket[MAX_UDP_PACKET_SIZE + 1];
+TinyXML xml;
+uint8_t buffer[150];  // For XML decoding
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// The running average of the measurement, in AD units
+RunningAverage runningAverage(runningAverageSize);
+
+void setup() {
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  // ESP8266 wierdism... there's a shadow EEPROM we manipulate
+  EEPROM.begin(256);
+
+  // Relay control for CW/CCW
+  pinMode(12, OUTPUT);
+  pinMode(14, OUTPUT);
+
+  moving = false;
+  // try to connect to wireless
+  WiFi.begin(getWifiSSID().c_str(), getWifiPassword().c_str());
+  if (testWifi()) {
+    Serial.println("Succesfully Connected!!!");
+    setupOta();
+    setupRoutes();
+    WebSerial.begin(&server);
+    WebSerial.msgCallback(receiveSerialMessage);
+    server.begin();
+    // Listener from RL
+    RLInfo.begin(localUdpPort);
+    // prime the running average
+    for (int i = 0; i < runningAverageSize; i++) {
+      runningAverage.addValue(analogRead(A0));
+      delay(25);
+    }
+    // set up xml processing
+    xml.init((uint8_t*)buffer, sizeof(buffer), &RL_XML_callback);
+
+  } else {  // not configured or can't connect
+    Serial.println("Turning the HotSpot On");
+    launchConfiguration();
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+}
+
+int counter = 0;
+int measurementTimer = 0;
+
+void loop() {
+  counter++;
+  measurementTimer++;
+
+  if (measurementTimer > 1000) {
+    measurementTimer = 0;
+    // Keep a running average
+    runningAverage.addValue(analogRead(A0));
+    ArduinoOTA.handle();
+  }
+  handleRLInfo();
+
+  if (counter > 10000) {
+    counter = 0;
+
+    if (!moving) {
+      return;
+    }
+
+    // watchdog timer
+    if (millis() > startedMovingAtMillis + watchdogTimeoutMillis) {
+      WebSerial.println("Watchdog timer expired!");
+      allStop();
+      return;
+    }
+
+    int heading = potToHeading(currentAverage());
+
+    Serial.printf("%d - %d is %d\n", heading, desiredHeading, heading - desiredHeading);
+    if (abs(heading - desiredHeading) > 5) {
+      if (heading > desiredHeading) {
+        Serial.println("go left");
+        digitalWrite(12, HIGH);
+        digitalWrite(14, LOW);
+      } else {
+        Serial.println("go right");
+        digitalWrite(12, LOW);
+        digitalWrite(14, HIGH);
+      }
+    } else {
+      allStop();
+      Serial.println("All stop!");
+    }
+  }
+}
+
+// Web serial callback for input. Doesn't do anything for now
+void receiveSerialMessage(uint8_t* data, size_t len) {
+  WebSerial.println("Received Web Data...");
+  String d = "";
+  for (int i = 0; i < len; i++) {
+    d += char(data[i]);
+  }
+  WebSerial.println(d);
+}
+
+
+// Stop the motor
+void allStop() {
+  digitalWrite(12, LOW);
+  digitalWrite(14, LOW);
+  WebSerial.println("All Stop!");
+  moving = false;
+}
+
+void setupOta() {
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(hostname);
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+}
+
+void setupRoutes() {
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send_P(200, "text/html", index_html, processor);
+    WebSerial.println("Root page request");
+  });
+
+  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest* request) {
+    String inputMessage1;
+    String inputMessage2;
+    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+    if (request->hasParam(PARAM_OUTPUT) && request->hasParam(PARAM_STATE)) {
+      inputMessage1 = request->getParam(PARAM_OUTPUT)->value();
+      inputMessage2 = request->getParam(PARAM_STATE)->value();
+      digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
+    } else {
+      inputMessage1 = "No message sent";
+      inputMessage2 = "No message sent";
+    }
+    Serial.print("GPIO: ");
+    Serial.print(inputMessage1);
+    Serial.print(" - Set to: ");
+    Serial.println(inputMessage2);
+    WebSerial.printf("Manual %s %s\n", inputMessage1, inputMessage2);
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/goto", HTTP_GET, [](AsyncWebServerRequest* request) {
+    String heading;
+    // GET input1 value on <ESP_IP>/goto?heading=degrees
+    if (request->hasParam("heading")) {
+      heading = request->getParam("heading")->value();
+      goTo(heading.toInt());
+    } else {
+      heading = "No message sent";
+    }
+    Serial.println("heading to " + heading);
+    request->send(200, "text/plain", "OK");
+  });
+}
+
+// Sets us up to move to a particular position
+void goTo(int heading) {
+  WebSerial.printf("Changing heading to %d\n", heading);
+  moving = true;
+  startedMovingAtMillis = millis();  // Log the time we started
+  desiredHeading = heading;
+}
+
+
+// UDP processing of port 12060
+void handleRLInfo() {
+  int packetSize = RLInfo.parsePacket();
+  if (packetSize) {
+    int len = RLInfo.read(incomingPacket, MAX_UDP_PACKET_SIZE);
+    xml.reset();
+    for (int i = 0; i < len; i++) {
+      xml.processChar(incomingPacket[i]);
+    }
+  }
+}
+
+void RL_XML_callback(uint8_t statusflags, char* tagName, uint16_t tagNameLen, char* data, uint16_t dataLen) {
+  if ((statusflags & STATUS_TAG_TEXT) && !strcasecmp(tagName, "/RotorCommand/TurnTo")) {
+    WebSerial.printf("Received UDP Packet to change to %s\n", data);
+    goTo(atoi(data));
+  }
+  if ((statusflags & STATUS_TAG_TEXT) && !strcasecmp(tagName, "/Contest_RotorCommand/TurnTo")) {
+    WebSerial.printf("Received UDP Contest Packet to change to %s\n", data);
+    goTo(atoi(data));
+  }
+}
+
+// Replaces placeholders in a page with dynamic info
+String processor(const String& var) {
+  //Serial.println(var);
+  if (var == "BUTTONPLACEHOLDER") {
+    String buttons = "";
+    buttons += "<h4>Output - GPIO 14</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"14\" " + outputState(14) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Output - GPIO 12</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"12\" " + outputState(12) + "><span class=\"slider\"></span></label>";
+    return buttons;
+  }
+  if (var == "DIRECTION") {
+    return String(potToHeading(currentAverage())) + String(" degrees, or " + String(currentAverage()));
+  }
+  if (var == "DESIREDHEADING") {
+    return String(desiredHeading) + String(moving ? " and moving" : " not moving");
+  }
+
+  return String();
+}
+
+String outputState(int output) {
+  if (digitalRead(output)) {
+    return "checked";
+  } else {
+    return "";
+  }
+}
+/*
+ Returns the current heading in degrees
+ Current measurements are
+ 450 - 644 / 360 - 670 / 270 - 703 / 180 - 735 / 90 - 772 / 0 - 814
+ */
+int potToHeading(int a) {
+  if (a >= 772) {
+    return map(a, 814, 772, 0, 90);
+  } else if (a >= 735) {
+    return map(a, 772, 735, 90, 180);
+  } else if (a >= 703) {
+    return map(a, 735, 703, 180, 270);
+  } else if (a > 670) {
+    return map(a, 703, .670, 270, 360);
+  } else {
+    return map(a, 814, 670, 0, 359);
+  }
+}
+
+int currentAverage() {
+  return runningAverage.getAverage();
+}
---
+++ autoconfig.ino
@@ -0,0 +1,137 @@
+String foundNetworks;
+
+String getWifiSSID() {
+  String ssid = "";
+  for (int i = 0; i < 32; ++i) {
+    ssid += char(EEPROM.read(i));
+  }
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  return ssid;
+}
+
+String getWifiPassword() {
+  String password = "";
+  for (int i = 32; i < 96; ++i) {
+    password += char(EEPROM.read(i));
+  }
+  Serial.print("Password: ");
+  Serial.println(password);
+  return password;
+}
+
+// If there's no value (default 255) in the first position, we've never been configured
+bool isConfigured() {
+  return EEPROM.read(0) != 255;
+}
+
+bool testWifi(void) {
+  int c = 0;
+  if (!isConfigured()) {
+    return false;
+  }
+
+  Serial.println("Waiting for Wifi to connect");
+  while (c < 20) {
+    if (WiFi.status() == WL_CONNECTED) {
+      return true;
+    }
+    delay(500);
+    Serial.print("*");
+    c++;
+  }
+  Serial.println("");
+  Serial.println("Connect timed out, opening AP");
+  return false;
+}
+
+void launchConfiguration() {
+  createWebServer();
+  server.begin();
+  setupAP();
+}
+
+void setupAP(void) {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.printf("There are %d networks\n", n);
+  if (n == 0) {
+    Serial.println("no networks found");
+    foundNetworks = "No networks found";
+  } else {
+    foundNetworks = "<ol>";
+    for (int i = 0; i < n; i++) {
+      // Print SSID and RSSI for each network found
+      foundNetworks += "<li>";
+      foundNetworks += WiFi.SSID(i);
+      foundNetworks += " (";
+      foundNetworks += WiFi.RSSI(i);
+
+      foundNetworks += ")";
+      foundNetworks += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+      foundNetworks += "</li>";
+    }
+    foundNetworks += "</ol>";
+  }
+
+  delay(100);
+  WiFi.softAP("how2electronics", "");
+}
+
+void createWebServer() {
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    IPAddress ip = WiFi.softAPIP();
+    String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+
+    String content;
+
+    content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+    content += ipStr;
+    content += "!!!<p>";
+    content += foundNetworks;
+    content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+    content += "</html>";
+    request->send(200, "text/html", content);
+  });
+
+  server.on("/setting", HTTP_GET, [](AsyncWebServerRequest* request) {
+    String qsid;
+    String qpass;
+
+    if (request->hasParam("ssid") && request->hasParam("pass")) {
+      qsid = request->getParam("ssid")->value();
+      qpass = request->getParam("pass")->value();
+
+
+      Serial.println("clearing eeprom");
+      for (int i = 0; i < 96; ++i) {
+        EEPROM.write(i, 0);
+      }
+      Serial.println(qsid);
+      Serial.println("");
+      Serial.println(qpass);
+      Serial.println("");
+
+      Serial.println("writing eeprom ssid:");
+      for (int i = 0; i < qsid.length(); i++) {
+        EEPROM.write(i, qsid[i]);
+        Serial.print("Wrote: ");
+        Serial.println(qsid[i]);
+      }
+      Serial.println("writing eeprom pass:");
+      for (int i = 0; i < qpass.length(); i++) {
+        EEPROM.write(32 + i, qpass[i]);
+        Serial.print("Wrote: ");
+        Serial.println(qpass[i]);
+      }
+      EEPROM.commit();
+      request->send(200, "application/json", "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}");
+      ESP.reset();
+    } else {
+      request->send(409, "application/json", "{\"Error\":\"Missing a ssid and password\"}");
+    }
+  });
+}
